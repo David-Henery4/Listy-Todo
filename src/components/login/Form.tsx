1db9ae2dd44c95f8a9bc2587fc9e1cc644ef6d90
DizzlyD <david4henery00@hotmail.com>
2024-07-25 22:32:59 +0100

    typo

--- src/components/login/Form.tsx
+++ src/components/login/Form.tsx
@@ -148,6 +148,7 @@ const Form = () => {
           e.preventDefault();
           checkInputValidation();
           if (errorStates.isError) return;
+          
           handleFormSubmit();
         }}
       >
